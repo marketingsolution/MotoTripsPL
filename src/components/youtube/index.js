5b97588ec98e443c01c6c3a37012d53f6458b9dd
Sofonias Abathun <sofonias@sweetopia.se>
2022-03-22 15:28:43 -0400

    Fix: unmatchable embed urls from crashing build script

--- src/components/youtube/index.js
+++ src/components/youtube/index.js
@@ -3,106 +3,108 @@ import React from "react"
 import { Helmet } from "react-helmet"
 
 export default function YouTube({ src }) {
-  const videoId = src
-    .match(
-      /^https?\:\/\/(?:www\.youtube(?:\-nocookie)?\.com\/|m\.youtube\.com\/|youtube\.com\/)?(?:ytscreeningroom\?vi?=|youtu\.be\/|vi?\/|user\/.+\/u\/\w{1,2}\/|embed\/|watch\?(?:.*\&)?vi?=|\&vi?=|\?(?:.*\&)?vi?=)([^#\&\?\n\/<>"']*)/i
-    )[1]
-    .trim()
+  const videoParts = src.match(
+    /^https?\:\/\/(?:www\.youtube(?:\-nocookie)?\.com\/|m\.youtube\.com\/|youtube\.com\/)?(?:ytscreeningroom\?vi?=|youtu\.be\/|vi?\/|user\/.+\/u\/\w{1,2}\/|embed\/|watch\?(?:.*\&)?vi?=|\&vi?=|\?(?:.*\&)?vi?=)([^#\&\?\n\/<>"']*)/i
+  )
 
-  return (
-    <>
-      <iframe
-        width="100%"
-        height="315"
-        src={src}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-      <StaticQuery
-        query={graphql`
-          {
-            youtube {
-              schema {
-                id
-                publishedAt
-                uploadDate
-                channelId
-                title
-                name
-                description
-                channelTitle
-                categoryId
-                liveBroadcastContent
-                defaultAudioLanguage
-                thumbnails {
-                  default {
-                    height
-                    url
-                    width
-                  }
-                  high {
-                    height
-                    url
-                    width
-                  }
-                  maxres {
-                    height
-                    url
-                    width
-                  }
-                  medium {
-                    height
-                    url
-                    width
-                  }
-                  standard {
-                    height
-                    url
-                    width
-                  }
-                }
-                thumbnailUrl
-                tags
-                localized {
-                  description
+  if (Array.isArray(videoParts) && videoParts.length > 1) {
+    const videoId = videoParts[1].trim()
+
+    return (
+      <>
+        <iframe
+          width="100%"
+          height="315"
+          src={src}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+        <StaticQuery
+          query={graphql`
+            {
+              youtube {
+                schema {
+                  id
+                  publishedAt
+                  uploadDate
+                  channelId
                   title
+                  name
+                  description
+                  channelTitle
+                  categoryId
+                  liveBroadcastContent
+                  defaultAudioLanguage
+                  thumbnails {
+                    default {
+                      height
+                      url
+                      width
+                    }
+                    high {
+                      height
+                      url
+                      width
+                    }
+                    maxres {
+                      height
+                      url
+                      width
+                    }
+                    medium {
+                      height
+                      url
+                      width
+                    }
+                    standard {
+                      height
+                      url
+                      width
+                    }
+                  }
+                  thumbnailUrl
+                  tags
+                  localized {
+                    description
+                    title
+                  }
+                  viewCount
+                  regionsAllowed
                 }
-                viewCount
-                regionsAllowed
               }
             }
-          }
-        `}
-        render={data => {
-          const schema =
-            Array.isArray(data.youtube.schema) &&
-            data.youtube.schema.find(schema => schema.id === videoId)
-          const { id, viewCount, ...rest } = schema
+          `}
+          render={data => {
+            const schema =
+              Array.isArray(data.youtube.schema) &&
+              data.youtube.schema.find(schema => schema.id === videoId)
+            const { id, viewCount, ...rest } = schema
 
-          return (
-            schema && (
-              <Helmet>
-                <script type="ld+json">
-                  {JSON.stringify({
-                    "@context": "https://schema.org",
-                    "@type": "VideoObject",
-                    contentUrl: `https://www.youtube.com/watch?v=${videoId}`,
-                    embedUrl: src,
-                    ...rest,
-                    interactionStatistic: {
-                      "@type": "InteractionCounter",
-                      interactionType: { "@type": "WatchAction" },
-                      userInteractionCount: viewCount,
-                    },
-                  })}
-                </script>
-              </Helmet>
+            return (
+              schema && (
+                <Helmet>
+                  <script type="ld+json">
+                    {JSON.stringify({
+                      "@context": "https://schema.org",
+                      "@type": "VideoObject",
+                      contentUrl: `https://www.youtube.com/watch?v=${videoId}`,
+                      embedUrl: src,
+                      ...rest,
+                      interactionStatistic: {
+                        "@type": "InteractionCounter",
+                        interactionType: { "@type": "WatchAction" },
+                        userInteractionCount: viewCount,
+                      },
+                    })}
+                  </script>
+                </Helmet>
+              )
             )
-          )
-        }}
-      />
-    </>
-  )
+          }}
+        />
+      </>
+    )
+  }
 }
